46b581d449f3749a430d08d86ed1383b43e9dda1
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-03 20:41:26 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyFirstAndroidApp_rollriera"
+include(":app")
