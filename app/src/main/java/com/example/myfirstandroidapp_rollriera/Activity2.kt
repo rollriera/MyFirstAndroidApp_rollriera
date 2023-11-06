79f5f33bda3e9bc7eac49db5be1fe522cc7a502d
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-06 21:59:22 +0900

    코틀린의 변수 와 상수 생성, 조건문

---
+++ app/src/main/java/com/example/myfirstandroidapp_rollriera/Activity2.kt
@@ -0,0 +1,37 @@
+package com.example.myfirstandroidapp_rollriera
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myfirstandroidapp_rollriera.databinding.ActivityMainBinding
+
+class Activity2: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
+
+        val myName = "상수"
+        var myAge = "변수"
+
+        //myName = "변경"
+        //myAge = "변경"
+
+        Log.d("상수",myName)
+        Log.d("변수",myAge)
+
+        val userAge = 25
+
+        if(userAge >= 20){
+            Log.d("사용자의나이","성인입니다.");
+        }else{
+            Log.d("사용자의 나이","학생입니다.");
+        }
+
+    }
+}
