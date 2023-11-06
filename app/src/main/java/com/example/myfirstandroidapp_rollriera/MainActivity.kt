6c4e1d87eacd6489b5b6570d04a86fe32d5dbb5d
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-06 17:40:59 +0900

    코틀린 기초 : viewBinding 을통해 태그에 접근, Log로 출력, Toast를 통해 사용자에게 출력

--- app/src/main/java/com/example/myfirstandroidapp_rollriera/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp_rollriera/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.myfirstandroidapp_rollriera
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.myfirstandroidapp_rollriera.databinding.ActivityMainBinding
 
 
@@ -15,8 +16,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // 확인버튼 이벤트 핸들러
         binding.OkBtn.setOnClickListener {
             Log.d("메인화면","버튼 눌림")
+            Log.e("메인화면","에러")
+        }
+
+        // 취소버튼 이벤트 핸들러
+        binding.cleBtn.setOnClickListener {
+            Log.d("메인화면","취소 버튼 눌림")
+            Toast.makeText(this, "작은 버튼 눌림", Toast.LENGTH_SHORT).show()
+
         }
 
 
