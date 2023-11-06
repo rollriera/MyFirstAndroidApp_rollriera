1f4b2772f0821601f2e5743a0d796d895550b453
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-07 00:07:10 +0900

    간단한 변수와 상수 선언 방법 Log를 통한 출력, 조건문을 통한 조건절에 따른 로직 Log로 출력, text값을 불러와 textView에 내용으로 반영

--- app/src/main/java/com/example/myfirstandroidapp_rollriera/Activity2.kt
+++ app/src/main/java/com/example/myfirstandroidapp_rollriera/Activity2.kt
@@ -33,5 +33,18 @@ class Activity2: AppCompatActivity() {
             Log.d("사용자의 나이","학생입니다.");
         }
 
+        binding.okBtn.setOnClickListener{
+
+
+            //입력된 내용 : contentEdt 의 text 값 조회 변수에 저장(get)
+            var inputContent = binding.contentEdt.text.toString();
+
+//            Log.d("inputContent입력내용",inputContent)
+
+            //저장해둔 입력값을 => 텍스트뷰의 내용으로 반영(set)
+            binding.resultTxt.text = inputContent;
+
+        }
+
     }
 }
