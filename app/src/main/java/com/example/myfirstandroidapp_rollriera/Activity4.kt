05f9e36166a393fe55ad385ade127e0e298f93a6
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-07 18:01:05 +0900

    MainActivity와 다른 Activity를 Intent를 통해 이동

--- app/src/main/java/com/example/myfirstandroidapp_rollriera/Activity4.kt
+++ app/src/main/java/com/example/myfirstandroidapp_rollriera/Activity4.kt
@@ -1,30 +1,23 @@
 package com.example.myfirstandroidapp_rollriera
 
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import com.example.myfirstandroidapp_rollriera.databinding.Activity4Binding
 import com.example.myfirstandroidapp_rollriera.databinding.ActivityMainBinding
 
+class Activity4 : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var binding : Activity4Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = Activity4Binding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        // 다른 화면으로 이동
-        binding.moveToOtherBtn.setOnClickListener {
-            val myIntent = Intent(this, Activity4::class.java)
-            startActivity(myIntent)
+        binding.returnToMainBtn.setOnClickListener {
+            finish()
         }
-
     }
-
-
 }
