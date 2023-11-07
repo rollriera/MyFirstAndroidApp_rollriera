a680e50053d73d10138a9ea9744223b98a2dfb6a
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-07 15:25:39 +0900

    간단한 변수&상수 선언 및 Log 출력, 조건문

--- app/src/main/java/com/example/myfirstandroidapp_rollriera/Activity2.kt
+++ app/src/main/java/com/example/myfirstandroidapp_rollriera/Activity2.kt
@@ -16,35 +16,35 @@ class Activity2: AppCompatActivity() {
         var view = binding.root
         setContentView(view)
 
-        val myName = "상수"
-        var myAge = "변수"
-
-        //myName = "변경"
-        //myAge = "변경"
-
-        Log.d("상수",myName)
-        Log.d("변수",myAge)
-
-        val userAge = 25
-
-        if(userAge >= 20){
-            Log.d("사용자의나이","성인입니다.");
-        }else{
-            Log.d("사용자의 나이","학생입니다.");
-        }
-
-        binding.okBtn.setOnClickListener{
-
-
-            //입력된 내용 : contentEdt 의 text 값 조회 변수에 저장(get)
-            var inputContent = binding.contentEdt.text.toString();
-
-//            Log.d("inputContent입력내용",inputContent)
-
-            //저장해둔 입력값을 => 텍스트뷰의 내용으로 반영(set)
-            binding.resultTxt.text = inputContent;
-
-        }
-
-    }
+//        val myName = "상수"
+//        var myAge = "변수"
+//
+//        //myName = "변경"
+//        //myAge = "변경"
+//
+//        Log.d("상수",myName)
+//        Log.d("변수",myAge)
+//
+//        val userAge = 25
+//
+//        if(userAge >= 20){
+//            Log.d("사용자의나이","성인입니다.");
+//        }else{
+//            Log.d("사용자의 나이","학생입니다.");
+//        }
+//
+//        binding.okBtn.setOnClickListener{
+//
+//
+//            //입력된 내용 : contentEdt 의 text 값 조회 변수에 저장(get)
+//            var inputContent = binding.contentEdt.text.toString();
+//
+////            Log.d("inputContent입력내용",inputContent)
+//
+//            //저장해둔 입력값을 => 텍스트뷰의 내용으로 반영(set)
+//            binding.resultTxt.text = inputContent;
+//
+//        }
+//
+   }
 }
