69171588b5e11ca3b85f14d7ec9d752abceef13a
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-07 17:43:00 +0900

    로그인 버튼에 이벤트를 걸어서 Toast로 사용자에게 로그인 여부 출력해주기(실습)

--- app/src/main/java/com/example/myfirstandroidapp_rollriera/Activity3.kt
+++ app/src/main/java/com/example/myfirstandroidapp_rollriera/Activity3.kt
@@ -1,7 +1,6 @@
 package com.example.myfirstandroidapp_rollriera
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myfirstandroidapp_rollriera.databinding.ActivityMainBinding
@@ -15,16 +14,16 @@ class Activity3 : AppCompatActivity() {
         var view = binding.root
         setContentView(view)
 
-        binding.loginBtn.setOnClickListener {
-            var id = binding.id.text.toString().equals("admin@test.com")
-            var pwd = binding.pwd.text.toString().equals("qwer")
-
-            if(id && pwd){
-                Toast.makeText(this, "관리자 입니다.", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
-            }
-        }
+//        binding.loginBtn.setOnClickListener {
+//            var id = binding.id.text.toString().equals("admin@test.com")
+//            var pwd = binding.pwd.text.toString().equals("qwer")
+//
+//            if(id && pwd){
+//                Toast.makeText(this, "관리자 입니다.", Toast.LENGTH_SHORT).show()
+//            }else{
+//                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+//            }
+//        }
 
     }
 }
