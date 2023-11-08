43079c4caae56a83d4826d7472e398aa668754d5
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-08 17:33:47 +0900

    Adepter클래스 뼈대 구축 ArrayAdapter클래스 상속받아 getView함수 오버라이딩을 통해 xml모양 반영

---
+++ app/src/main/java/com/example/myfirstandroidapp_rollriera/adapters/StudentAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.myfirstandroidapp_rollriera.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.myfirstandroidapp_rollriera.R
+import com.example.myfirstandroidapp_rollriera.datas.Students
+import java.util.Calendar
+
+class StudentAdapter(val mContext : Context,
+                     val resId : Int,
+                     val mList : ArrayList<Students>) : ArrayAdapter<Students>(mContext,resId,mList) {
+
+     val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val studentData = mList[position]
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val birthYear = row.findViewById<TextView>(R.id.birthYearTxt)
+
+        val caleander = Calendar.getInstance();
+        val nowYear = caleander.get(Calendar.YEAR)
+
+        var age = nowYear - mList[position].birthYear
+
+        nameTxt.text = mList[position].name
+        birthYear.text = "(${age}세)"
+
+        return row
+
+    }
+
+
+}
