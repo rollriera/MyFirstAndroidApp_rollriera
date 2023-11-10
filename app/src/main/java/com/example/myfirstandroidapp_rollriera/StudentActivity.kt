ac36a89652fc845c5c8c8a4be2286ae458b40b4b
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-10 18:58:03 +0900

    Adapter패턴을 이용해 ListView에 연결하여 학생 정보를 띄움 setOnItemClickListener, setOnItemLongClickListener사용하여 간단한 Toast함수 이용하여 이벤트 핸들러가 잘 실행되는지 출력

--- app/src/main/java/com/example/myfirstandroidapp_rollriera/StudentActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp_rollriera/StudentActivity.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import com.example.myfirstandroidapp_rollriera.databinding.ActivityStudentBinding
 
 class StudentActivity : AppCompatActivity() {
-    val mStudentList = ArrayList<Students>()
+//    val mStudentList = ArrayList<Students>()
 
-    lateinit var mAdapter : StudentAdapter
+//    lateinit var mAdapter : StudentAdapter
     private lateinit var binding: ActivityStudentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,38 +15,38 @@ class StudentActivity : AppCompatActivity() {
         var view = binding.root
         setContentView(view)
 
-        mStudentList.add(Students("홍길동",1999))
-        mStudentList.add(Students("둘리",1979))
-        mStudentList.add(Students("또치",1989))
-        mStudentList.add(Students("하니",2000))
-        mStudentList.add(Students("길동",2003))
-        mStudentList.add(Students("보라돌이",1969))
-        mStudentList.add(Students("뚜비",2001))
-
-        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
-
-        binding.studentListView.adapter = mAdapter
-
-        // 리스트의 이벤트(그냥 눌렀을때의 이벤트)
-        binding.studentListView.setOnItemClickListener { adapterView, view, position, id ->
-
-            val clickedStudent = mStudentList[position].name
-
-            Toast.makeText(this, "${clickedStudent}", Toast.LENGTH_SHORT).show()
-
-        }
-
-        // 리스트의 이벤트(길게 눌렀을때의 이벤트)
-        binding.studentListView.setOnItemLongClickListener { adapterView, view, position, id ->
-
-            //리스트 삭제(그냥 실행하면 앱이 죽어버림)
-            mStudentList.removeAt(position)
-
-            mAdapter.notifyDataSetChanged()
-
-
-            return@setOnItemLongClickListener true
-        }
+//        mStudentList.add(Students("홍길동",1999))
+//        mStudentList.add(Students("둘리",1979))
+//        mStudentList.add(Students("또치",1989))
+//        mStudentList.add(Students("하니",2000))
+//        mStudentList.add(Students("길동",2003))
+//        mStudentList.add(Students("보라돌이",1969))
+//        mStudentList.add(Students("뚜비",2001))
+//
+//        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+//
+//        binding.studentListView.adapter = mAdapter
+//
+//        // 리스트의 이벤트(그냥 눌렀을때의 이벤트)
+//        binding.studentListView.setOnItemClickListener { adapterView, view, position, id ->
+//
+//            val clickedStudent = mStudentList[position].name
+//
+//            Toast.makeText(this, "${clickedStudent}", Toast.LENGTH_SHORT).show()
+//
+//        }
+//
+//        // 리스트의 이벤트(길게 눌렀을때의 이벤트)
+//        binding.studentListView.setOnItemLongClickListener { adapterView, view, position, id ->
+//
+//            //리스트 삭제(그냥 실행하면 앱이 죽어버림)
+//            mStudentList.removeAt(position)
+//
+//            mAdapter.notifyDataSetChanged()
+//
+//
+//            return@setOnItemLongClickListener true
+//        }
 
     }
 }
