75af505b9f8ff1c4a1c020327066458530554ed7
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-13 12:25:17 +0900

    Handler를 통해서 getDelayed함수 사용하여 2.5초 후 MainActivity로 이동

---
+++ app/src/main/java/com/example/myfirstandroidapp_rollriera/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myfirstandroidapp_rollriera
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.myfirstandroidapp_rollriera.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+//        각종 데이터 확인 => 앱 실행전에  필요한 사전 작업 수행.
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+                 // 2.5초 후에 실행할 내용 작성(메인화면으로 이동 + 지금 화면 종료)
+            val myIntent = Intent(this,MainActivity::class.java)
+
+            startActivity(myIntent)
+
+            finish()
+        }, 2500)
+    }
+}
